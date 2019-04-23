defecf0a4df712923e290e49944eede5d9c97e9b
Monnyka <31350922+Monnyka@users.noreply.github.com>
2019-04-23 23:30:56 +0700

    Update Main Screen, Update top scroll view

--- app/src/main/java/com/nyka/primedb/MainActivity.java
+++ app/src/main/java/com/nyka/primedb/MainActivity.java
@@ -7,26 +7,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button movie_detail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        movie_detail.findViewById(R.id.ll_Button_Brows);
-
-        movie_detail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                OpenScreenDetail();
-            }
-        });
-    }
-
-    public void OpenScreenDetail(){
-        Intent intent =new Intent(this, activity_moviedetail.class);
-        startActivity(intent);
     }
 
 }
