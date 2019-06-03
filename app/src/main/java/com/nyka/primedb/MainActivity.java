fe69e77dbc5faeac010414b571cd576e37484a26
Monnyka <31350922+Monnyka@users.noreply.github.com>
2019-06-03 21:33:08 +0700

    Update screen moviedetail

--- app/src/main/java/com/nyka/primedb/MainActivity.java
+++ app/src/main/java/com/nyka/primedb/MainActivity.java
@@ -5,14 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView=(ImageView) findViewById(R.id.ivComing1);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenDetailActivity();
+            }
+        });
+
+
+
+    }
+    public void OpenDetailActivity(){
+        Intent intent = new Intent(this, activity_moviedetail.class);
+        startActivity(intent);
     }
 
 }
